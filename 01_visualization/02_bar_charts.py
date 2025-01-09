7a816ebfee6f392cc24d6f8df24595a739298871
Well Pharmacy <wellpharmacy04@gmail.com>
2025-01-09 06:20:29 +0000

    Add grade distribution histogram plotting function

--- 01_visualization/02_bar_charts.py
+++ 01_visualization/02_bar_charts.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from typing import List
+from collections import Counter
 
 
 def plot_movie_oscars(
@@ -28,7 +29,33 @@ def plot_movie_oscars(
     plt.close()
 
 
+def plot_grade_distribution(grades: List[int]):
+    """
+    Plots a histogram of grades.
+
+    Parameters:
+    grades (list of int): List of grades.
+    """
+    histogram = Counter(min(grade // 10 * 10, 90) for grade in grades)
+    plt.bar(
+        [x + 5 for x in histogram.keys()],  # Shift bars right by 5
+        histogram.values(),  # Give each bar its correct height
+        10,  # Give each bar a width of 10
+        edgecolor=(0, 0, 0),
+    )  # Black edges for each bar
+    plt.axis([-5, 105, 0, 5])  # x-axis from -5 to 105, y-axis from 0 to 5
+    plt.xticks([10 * i for i in range(11)])  # x-axis labels at 0, 10, ..., 100
+    plt.xlabel("Decile")
+    plt.ylabel("# of Students")
+    plt.title("Distribution of Exam 1 Grades")
+    plt.savefig("grade_distribution.png", dpi=300)
+    plt.close()
+
+
 if __name__ == "__main__":
     movies = ["Annie Hall", "Ben-Hur", "Casablanca", "Gandhi", "West Side Story"]
     num_oscars = [5, 11, 3, 8, 10]
     plot_movie_oscars(movies, num_oscars)
+
+    grades = [83, 95, 91, 87, 70, 0, 85, 82, 100, 67, 73, 77, 0]
+    plot_grade_distribution(grades)
